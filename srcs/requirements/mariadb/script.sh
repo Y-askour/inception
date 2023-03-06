c3050010cc2f86ed4c7da0bb60e710dd5944ba94
Younes Askour <pausedarkf@gmail.com>
2023-03-06 15:48:00 +0100

    deploying nginx using docker-compose && and working on mariadb

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -2,6 +2,7 @@
 
 service mysql start
 
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD';" >> database.sql
 echo "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;" > database.sql
 
 #set root with the password 123456789
@@ -16,4 +17,8 @@ echo "GRANT ALL ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';" >> database.sql
 #reloads the privileges from the grant tables in the MySQL database
 echo "FLUSH PRIVILEGES;" >> database.sql
 
-mysql < database.sql
+mysqld < database.sql
+
+kill `cat /var/run/mysqld/mysqld.pid`
+
+mysqld-safe
