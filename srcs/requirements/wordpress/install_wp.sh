2b6061a386bb321eb5b827a9b63d6db50593a4e3
Younes Askour <pausedarkf@gmail.com>
2023-03-14 14:37:38 +0100

    allow mariadb to get requests outside the localhost

--- srcs/requirements/wordpress/install_wp.sh
+++ srcs/requirements/wordpress/install_wp.sh
@@ -3,4 +3,4 @@ cd /var/www/html
 wp-cli core download --allow-root
 wp-cli config create --allow-root --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=mariadb
 wp-cli core install --allow-root --url=localhost --title=test --admin_user=$admin_user --admin_password=$admin_pass --admin_email=$email
-while true; do sleep 1; done 
+php-fpm -F
