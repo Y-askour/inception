8fb6e14ec1b267ec7affd34fffe04457e612c061
Younes Askour <pausedarkf@gmail.com>
2023-03-17 13:56:18 -0400

    final product

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 all:
+	mkdir -p /home/yaskour/data/db
+	mkdir -p /home/yaskour/data/web
 	docker-compose -f ./srcs/docker-compose.yml build
 	docker-compose -f ./srcs/docker-compose.yml up -d
 clean:
 	docker-compose -f ./srcs/docker-compose.yml down -v
+
 fclean: clean
 	docker rmi $$(docker images -q)
--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -3,9 +3,8 @@ apt-get update -y
 apt-get install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
 apt-get install curl -y
 apt-get install php-mysql -y
-apt install php-fpm php7.3-fpm php-mysql curl -y
+apt install php-fpm php7.3-fpm curl -y
 apt-get install php -y
-apt-get install php-fpm -y
 apt-get install mariadb-client -y
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
